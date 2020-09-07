0d7b3ed3f7076fcdfd496fc6a94a21dfc19d92b3
DeeDowns <dee_downs@outlook.com>
2020-09-06 20:38:28 -0400

    database queries

--- api/server.js
+++ api/server.js
@@ -4,6 +4,18 @@ const db = require("../data/dbConfig.js");
 
 const server = express();
 
+server.use(logger)
 server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'server uppppp' })
+})
+
+
+function logger(req, res, next) {
+    console.log(`a ${req.method} request was made to ${req.url}`)
+    next()
+}
 
 module.exports = server;
